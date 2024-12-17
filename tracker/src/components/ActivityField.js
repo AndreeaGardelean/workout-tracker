69fa590ed07c54b397eb096c0b119b2d1579d46c
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-12-17 10:31:13 +0000

    Added an x to every activity field and an event listener for onCLick event. When the value x is clicked in the field, the corresponding field is deleted

--- tracker/src/components/ActivityField.js
+++ tracker/src/components/ActivityField.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "../style/activityField.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ActivityField = ({ activityId, activityLabel }) => {
+  const deleteField = (e) => {
+    const form = document.getElementById("workoutForm");
+    const target = e.target.closest(".activity");
+
+    if (target) {
+      form.removeChild(target);
+    }
+    // form.removeChild(e.target.closest(activityId));
+  };
+
   return (
     <div className="activity" id={activityId}>
       <input type="text" defaultValue={activityLabel} className="label"></input>
@@ -10,6 +22,13 @@ const ActivityField = ({ activityId, activityLabel }) => {
         id={activityId + "-input"}
         name={activityId + "-name"}
       ></input>
+      <div
+        className="deleteField"
+        onClick={deleteField}
+        id={"deleteBtn-" + activityId}
+      >
+        <FontAwesomeIcon icon={faPlus} />
+      </div>
     </div>
   );
 };
